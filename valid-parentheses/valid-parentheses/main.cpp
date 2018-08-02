a75d3a5cd7cd79e4d29b9f596a9b0ba999c1f1ef
tbago <moonzalor@gmail.com>
2018-08-02 09:31:29 +0800

    valid parentheses: add another solution. In my compile have the right result, but on line have different result.

--- valid-parentheses/valid-parentheses/main.cpp
+++ valid-parentheses/valid-parentheses/main.cpp
@@ -16,6 +16,35 @@ class Solution {
 public:
     bool isValid(string s) {
         int stringLength = (int)s.length();
+        if (stringLength == 0) {
+            return true;
+        }
+        if (stringLength%2 != 0 ) {
+            return false;
+        }
+        
+        int strBufferLength = (int)s.length() + 1;
+        char *strBuffer = (char *)malloc(strBufferLength + 1);
+        memset(strBuffer, 0, strBufferLength);
+        memcpy(strBuffer, s.c_str(), s.length());
+        int strLength = (int)s.length();
+        bool result = true;
+        while (true) {
+            bool findClosePair = remotePairParentheses(strBuffer, strLength);
+            if (!findClosePair) {
+                result = false;
+                break;
+            }
+            else if (strLength == 0) {
+                result = true;
+                break;
+            }
+        }
+        free(strBuffer);
+        return result;
+    }
+    bool isValidV2(string s) {
+        int stringLength = (int)s.length();
         
         if (stringLength%2 != 0 ) {
             return false;
@@ -49,6 +78,24 @@ private:
             return true;
         }
         return false;
+    }
+    
+    bool remotePairParentheses(char *strBuffer, int &strBufferLength) {
+        bool findClosePair = false;
+        for (uint32_t i = 0; i < strBufferLength-1; i++) {
+            char firstChar = *(strBuffer + i);
+            char secondChar = *(strBuffer + i + 1);
+            if (isPairParentheses(firstChar, secondChar)) {
+                findClosePair = true;
+                strBufferLength -= 2;
+                if (i < strBufferLength) {
+                    memcpy(strBuffer + i, strBuffer + i + 2, strBufferLength - i);
+                    memset(strBuffer+strBufferLength, 0, 2);
+                }
+                break;
+            }
+        }
+        return findClosePair;
     }
 };
 
